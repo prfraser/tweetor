ec80913805a5c9299efe598f0e38065c64df2ceb
philip fraser <pr.fraser3@gmail.com>
2017-09-25 18:02:50 +1000

    added first / last name to model, users tweets are public, can view profile pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925014841) do
+ActiveRecord::Schema.define(version: 20170925052605) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 20170925014841) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
