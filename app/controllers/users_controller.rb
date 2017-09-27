81e1e9ca82e0dce954f83b3528bcf4030de1cdad
philip fraser <pr.fraser3@gmail.com>
2017-09-27 15:34:04 +1000

    added following to user profile view page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by slug: params[:slug]
     @tweets = @user.tweets
-    @follows = current_user.all_follows
+    @follows = current_user.all_following
   end
 
 end
